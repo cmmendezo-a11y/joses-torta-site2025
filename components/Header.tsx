f53da962d7ec539b4331df9e136ce29154744b2f
cmmendezo-a11y <cmmendezo@gmail.com>
2025-10-31 18:56:38 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,8 +24,8 @@ export default function Header() {
           <Image
             src="../public/tortamexicanafinal.jpg"                 // <— usa el nombre real del archivo que subiste
             alt="Jose’s Torta Mexicana logo"
-            width={80}
-            height={80}
+            width={50}
+            height={50}
             priority
             className="h-10 w-10 rounded-xl object-contain"
           />
